b02f180416b6ee8506c7b36a823f9cbb713aef6d
DELL <nguyenxuanhuy2108@gmail.com>
2020-04-09 21:59:47 +0700

    update

---
+++ app/src/main/java/vn/edu/ntu/nguyenxuanhuy_58131311_helloword/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.ntu.nguyenxuanhuy_58131311_helloword;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
